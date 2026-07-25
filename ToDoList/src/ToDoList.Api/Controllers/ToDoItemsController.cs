3f1d5ae50abd0f89732e92d4ee2877b0a25f31ec
Akobir <akobirabduvahobov@gmail.com>
2026-07-25 17:56:24 +0500

    added

---
+++ ToDoList/src/ToDoList.Api/Controllers/ToDoItemsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Api.Controllers;
+
+[Route("api/v1/todoitems")]
+[ApiController]
+public class ToDoItemsController : ControllerBase
+{
+}
