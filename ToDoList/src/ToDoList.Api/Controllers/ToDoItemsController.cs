3a3dd322223154b6c53487e211fd4d73bbe8b578
Serikzhan0 <maxamatxanovvserikzhan@gmail.com>
2026-07-25 18:40:40 +0500

    Add search endpoint for todo items

--- ToDoList/src/ToDoList.Api/Controllers/ToDoItemsController.cs
+++ ToDoList/src/ToDoList.Api/Controllers/ToDoItemsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Application.Services;
+using ToDoList.Application.Dtos;
+using Microsoft.AspNetCore.Http; // Для StatusCodes
 
 namespace ToDoList.Api.Controllers;
 
@@ -7,4 +9,18 @@ namespace ToDoList.Api.Controllers;
 [ApiController]
 public class ToDoItemsController : ControllerBase
 {
+    private readonly IToDoItemService _toDoItemService;
+
+    public ToDoItemsController(IToDoItemService toDoItemService)
+    {
+        _toDoItemService = toDoItemService;
+    }
+
+    [HttpGet("search")]
+    [ProducesResponseType(typeof(List<ToDoItemDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Search([FromQuery] string searchTerm)
+    {
+        var result = await _toDoItemService.Search(searchTerm);
+        return Ok(result);
+    }
 }
