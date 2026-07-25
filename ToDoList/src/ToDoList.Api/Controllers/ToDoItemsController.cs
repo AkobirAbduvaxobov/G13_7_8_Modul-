105389f49841e9e9b45482517b2a531ee0cc7941
e1shod <hackergemer9@gmail.com>
2026-07-25 18:09:17 +0500

    add todoitem delate

--- ToDoList/src/ToDoList.Api/Controllers/ToDoItemsController.cs
+++ ToDoList/src/ToDoList.Api/Controllers/ToDoItemsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Entities;
+using ToDoList.Infrastructure.Persistence;
 
 namespace ToDoList.Api.Controllers;
 
@@ -7,4 +10,28 @@ namespace ToDoList.Api.Controllers;
 [ApiController]
 public class ToDoItemsController : ControllerBase
 {
+    private readonly AppDbContext _context;
+
+    public ToDoItemsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> Delete(long id)
+    {
+        var item = await _context.ToDoItems.FirstOrDefaultAsync(x => x.ToDoItemId == id && !x.IsDeleted);
+
+        if (item is null)
+        {
+            return NotFound(new { message = "ToDo item topilmadi." });
+        }
+
+        item.IsDeleted = true;
+        item.DeletedAt = DateTime.UtcNow;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 }
