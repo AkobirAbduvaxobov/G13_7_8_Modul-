2f5e772f7eed08293336309cd43bc91189c96b5c
Tohir <elektron_pochtangiz@example.com>
2026-07-25 18:42:45 +0500

    added get todoitems endpoint and service

---
+++ ToDoList/src/ToDoList.Application/Converters/ToDoItemConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Application.Dtos;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Application.Converters;
+
+public static class ToDoItemConverter
+{
+    public static ToDoItem ToEntity(this ToDoItemCreateDto dto, long userId)
+    {
+        return new ToDoItem
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Priority = dto.Priority,
+            DueDate = dto.DueDate,
+            ReminderAt = dto.ReminderAt,
+            UserId = userId,
+            IsCompleted = false,
+            IsDeleted = false,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+    }
+}
--- ToDoList/src/ToDoList.Application/Services/ToDoItemService.cs
+++ ToDoList/src/ToDoList.Application/Services/ToDoItemService.cs
@@ -1,6 +1,45 @@
-﻿namespace ToDoList.Application.Services
+﻿using System;
+using ToDoList.Application.Dtos;
+using ToDoList.Domain.Entities;
+using ToDoList.Application.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ToDoList.Application.Services;
+
+
+
+public class ToDoItemService : IToDoItemService
 {
-    public class ToDoItemService : IToDoItemService
+    private readonly IBaseRepository<ToDoItem> _toDoRepository;
+
+
+    public ToDoItemService(IBaseRepository<ToDoItem> toDoRepository)
     {
+        _toDoRepository = toDoRepository;
+    }
+
+    public async Task<ToDoItem> CreateAsync(ToDoItemCreateDto dto, long userId)
+    {
+        var entity = new ToDoItem
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Priority = dto.Priority,
+            DueDate = dto.DueDate,
+            ReminderAt = dto.ReminderAt,
+            UserId = userId,
+            IsCompleted = false,
+            IsDeleted = false,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+
+        await _toDoRepository.AddAsync(entity);
+        await _toDoRepository.SaveChangesAsync();
+
+        return entity;
     }
 }
+
